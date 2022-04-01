20b7b0042be774696deadf634a49d08c770816f2
Pete Seeling <peteseeling@gmail.com>
2022-04-01 16:22:45 -0600

    signs in existing user still failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,7 +16,7 @@ describe('top-secret-backend routes', () => {
 
   it('signs up a user with email/password via POST', async() => {
     const res = await request(app)
-      .post('/api/v1/users/sessions')
+      .post('/api/v1/users')
       .send({ email: 'pete@gmail.com', password:'pleaseWork' });
 
     expect(res.body).toEqual({ id: expect.any(String), email: 'pete@gmail.com', passwordHashed: expect.any(String) });
@@ -36,7 +36,6 @@ describe('top-secret-backend routes', () => {
       message: 'Signed in successfully!',
       user,
     });
- 
   });
   
 
