281988f902753762a402dea22a21bde4e8eba00f
Pete Seeling <peteseeling@gmail.com>
2022-04-09 15:26:21 -0600

    secrets post test written

---
+++ __tests__/secrets.test.js
@@ -0,0 +1,43 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/services/UserService');
+const UserService = require('../lib/services/UserService');
+
+describe('top-secret-routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should allow a user to post a secret', async () => {
+    const agent = request.agent(app);
+
+    await UserService.create({
+      email:'pete@gmail.com',
+      password:'pleaseWork',
+    });
+
+    await agent.post('api/v1/users/sessions')
+      .send({
+        email:'pete@gmail.com',
+        password:'pleaseWork',
+      });
+
+    const res = await agent.post('api/v1/secrets')
+      .send({
+        title:'New Secret',
+        description: 'new secret text'
+      });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      title:'New Secret',
+      description: 'new secret text',
+      createdAt: expect.any(String),
+    });
+  });
+});
