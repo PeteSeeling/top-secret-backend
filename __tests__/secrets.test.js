43a57cbed450dc2c693fa363f00dad10c288acec
Pete Seeling <peteseeling@gmail.com>
2022-04-09 19:37:18 -0600

    get all secrets test complete

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -1,8 +1,9 @@
 const pool = require('../lib/utils/pool');
+const UserService = require('../lib/services/UserService');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
+
 
 describe('top-secret-routes', () => {
   beforeEach(() => {
@@ -17,28 +18,25 @@ describe('top-secret-routes', () => {
    
     const agent = request.agent(app);
 
-    await agent
-      .post('/api/v1/users')
-      .send({
-        email:'pete@gmail.com',
-        password: 'pleaseWork',
-      });
-
-
+    await UserService.create({
+      email:'pete@gmail.com',
+      password: 'pleaseWork',
+    });
+  
     await agent
       .post('/api/v1/users/sessions')
-      .send({
+      .send({ 
         email:'pete@gmail.com',
         password: 'pleaseWork',
       });
-
+      
     const res = await agent
       .post('/api/v1/secrets')
       .send({
         title:'New Secret',
         description: 'new secret text'
       });
-
+      
     expect(res.body).toEqual({
       id: expect.any(String),
       title:'New Secret',
